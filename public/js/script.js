dd86669cbaacee8ca994de0fab71daf540f726a0
Dsopinka <96312744+Dsopinka@users.noreply.github.com>
2023-04-16 03:41:56 +0000

    Data saving

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ let countdownTimer;
 
 function saveUserData(data) {
   localStorage.setItem("userData", JSON.stringify(data));
+  console.log("saveUserData called with data:", data)
 }
 
 function loadUserDataFromLocalStorage() {
@@ -21,8 +22,16 @@ function getRemainingTime() {
   const hours = parseInt(timeParts[0], 10);
   const minutes = parseInt(timeParts[1], 10);
   const seconds = parseInt(timeParts[2], 10);
+
+  // Check if there is any remaining time stored in local storage
+  const data = loadUserDataFromLocalStorage();
+  if (data.remainingTime) {
+    return data.remainingTime;
+  }
+
   return hours * 3600 + minutes * 60 + seconds;
 }
+
 
 function loadUserData() {
   const data = loadUserDataFromLocalStorage();
@@ -51,25 +60,35 @@ function loadUserData() {
   }
 
   if (data.remainingTime) {
-    startCountdownTimer(data.remainingTime);
-  }
-
-  if (data.timerEndTime) {
+    remainingTime = data.remainingTime;
+  } else if (data.timerEndTime) {
     remainingTime = Math.max(
       0,
       Math.floor((new Date(data.timerEndTime) - new Date()) / 1000)
     );
+  }
 
+  if (remainingTime > 0) {
+    startCountdownTimer(remainingTime);
+  } else if (data.timerEndTime) {
+    const timerEndTime = new Date(data.timerEndTime);
+    remainingTime = Math.max(
+      0,
+      Math.floor((timerEndTime - new Date()) / 1000)
+    );
     startCountdownTimer(remainingTime);
   }
 }
 
+
 function startCountdownTimer(remainingTime) {
+  console.log("startCountdownTimer called with remainingTime:", remainingTime);
   const timerDisplay = document.getElementById("selected-option4");
 
   if (countdownTimer) {
     clearInterval(countdownTimer);
   }
+
 
   countdownTimer = setInterval(() => {
     if (remainingTime <= 0) {
@@ -92,50 +111,52 @@ function startCountdownTimer(remainingTime) {
   
   
   
-      function handleSubmit() {
-        const selectOneValue = document.querySelector(
-          'input[name="selectOne"]:checked'
-        ).value;
-        const formData = new FormData(document.querySelector("form"));
-        const timerEndTime = new Date(
-          Date.now() + getRemainingTime() * 1000
-        ).toISOString();
-      
-        // Clear the existing countdown timer
-        if (countdownTimer) {
-          clearInterval(countdownTimer);
-        }
-      
-        // Calculate the new remaining time based on the new duration selected by the user
-        
-        const durationHoursValue = parseInt(formData.get("tempHours"), 10) || 0;
-        const durationMinutesValue = parseInt(formData.get("tempMinutes"), 10) || 0;
-        let remainingTime = durationHoursValue * 3600 + durationMinutesValue * 60;
+function handleSubmit() {
+  const selectOneValue = document.querySelector(
+    'input[name="selectOne"]:checked'
+  ).value;
+  const formData = new FormData(document.querySelector("form"));
+  const timerEndTime = new Date(
+    Date.now() + getRemainingTime() * 1000
+  ).toISOString();
+
+  // Clear the existing countdown timer
+  if (countdownTimer) {
+    clearInterval(countdownTimer);
+  }
+
+  // Calculate the new remaining time based on the new duration selected by the user
+  const durationHoursValue = parseInt(formData.get("tempHours"), 10) || 0;
+  const durationMinutesValue = parseInt(formData.get("tempMinutes"), 10) || 0;
+  let remainingTime = durationHoursValue * 3600 + durationMinutesValue * 60;
+
+  console.log('durationHoursValue:', durationHoursValue);
+  console.log('durationMinutesValue:', durationMinutesValue);
 
 
-      
-        // Save the remaining time and timer end time in local storage
-        const data = {
-          selectOne: formData.get("selectOne"),
-          postalCode: formData.get("postalCode"),
-          oneOption: formData.get("oneOption"),
-          tempature: formData.get("tempature"),
-          tempHours: formData.get("tempHours"),
-          tempMinutes: formData.get("tempMinutes"),
-          remainingTime: remainingTime,
-          timerEndTime: timerEndTime,
-        };
-        saveUserData(data);
-      
-        // Load the user data from localStorage and update the page
-        loadUserData();
-      
-        startCountdownTimer(remainingTime);
+  // Save the remaining time and timer end time in local storage
+  const data = {
+    selectOne: selectOneValue,
+    postalCode: formData.get("postalCode"),
+    oneOption: formData.get("oneOption"),
+    tempature: formData.get("tempature"),
+    tempHours: formData.get("tempHours"),
+    tempMinutes: formData.get("tempMinutes"),
+    remainingTime: remainingTime,
+    timerEndTime: timerEndTime,
+  };
+  saveUserData(data);
 
-      
-        // Navigate back to the home page
-        window.history.pushState({}, "", "/");
-      }
+  // Load the user data from localStorage and update the page
+  loadUserData();
+
+
+  // Navigate back to the home page
+  window.history.pushState({}, "", "/");
+
+  console.log("handleSubmit called");
+}
+
 
       function resetForm() {
         // Reset form values
@@ -177,13 +198,22 @@ function startCountdownTimer(remainingTime) {
       }
       
       
-      
+    // Add event listener for popstate event to handle when user navigates back or forward
+    window.addEventListener("load", () => {
+      clearInterval(countdownTimer);
+      loadUserData();
+    });
+  
   
 
 
  //loads DOM
 window.addEventListener("DOMContentLoaded", (event) => {
   loadUserData();
+  if (countdownTimer) {
+    startCountdownTimer(getRemainingTime());
+  }
+  
   const dateDisplay = document.getElementById("date");
 // gets current time and date
   const date = new Date();
@@ -215,18 +245,48 @@ window.addEventListener("DOMContentLoaded", (event) => {
   //when button is clicked submits second set of options
   const form2 = document.querySelector(".postTemp");
   const submitButton3 = document.getElementById("submit-3");
-  submitButton3.addEventListener("click", (event) => {
-    event.preventDefault();
-    const selectedValue2 = document.querySelector(
-      'input[name="oneOption"]:checked'
-    ).value;
-    const selectedOption2 = document.getElementById("selected-option2");
-    selectedOption2.innerHTML = "<strong>" + selectedValue2 + "</strong>";
-    selectedOption2.textContent = selectedValue2;
-    
-  handleSubmit();
+submitButton3.addEventListener("click", (event) => {
+  event.preventDefault();
+  const selectedValue2 = document.querySelector(
+    'input[name="oneOption"]:checked'
+  ).value;
+  const selectedOption2 = document.getElementById("selected-option2");
+  selectedOption2.innerHTML = "<strong>" + selectedValue2 + "</strong>";
+  selectedOption2.textContent = selectedValue2;
 
-  });
+  /*// Get the values of the hours and minutes input elements
+  const tempHours = document.getElementById("hours").value;
+  const tempMinutes = document.getElementById("minutes").value;
+
+  // Start the countdown timer
+  const timerDisplay = document.getElementById("selected-option4");
+  const timerDuration = tempHours * 3600 + tempMinutes * 60; // duration in seconds
+  let remainingTime = timerDuration;
+
+  const timerInterval = setInterval(() => {
+    const initialTemp = tempInput.value;
+    const tempDisplay = document.getElementById("selected-option3");
+    const hours = Math.floor(remainingTime / 3600);
+    const minutes = Math.floor((remainingTime % 3600) / 60);
+    const seconds = remainingTime % 60;
+
+    timerDisplay.innerHTML = `${hours.toString().padStart(2, "0")}:${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    // checks if there is time remaining and if not resets to default value
+    if (remainingTime === 0) {
+      clearInterval(timerInterval);
+      timerDisplay.innerHTML = "";
+      tempDisplay.innerHTML = `${initialTemp}°C`;
+      tempInput.defaultValue
+    } else {
+      remainingTime--;
+    }
+  }, 1000);*/
+
+  handleSubmit();
+});
+
 //updates the temperature value from the temp slider
   const slider = document.querySelector("#temp-slider");
   const selectedOption3 = document.querySelector("#selected-option3");
@@ -248,43 +308,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
     const tempDisplay = document.getElementById("selected-option3");
     tempDisplay.innerHTML = `${tempValue}°C`;
     const initialTemp = tempInput.defaultValue;
-    console.log(submitButton3)
   
   });
   
-  
-    // Start the countdown timer
-    const timerDisplay = document.getElementById("selected-option4");
-    const timerDuration = durationHoursValue * 3600 + durationMinutesValue * 60; // duration in seconds
-    let remainingTime = timerDuration;
-  
-      const timerInterval = setInterval(() => {
-      const hours = Math.floor(remainingTime / 3600);
-      const minutes = Math.floor((remainingTime % 3600) / 60);
-      const seconds = remainingTime % 60;
-  
-      timerDisplay.innerHTML = `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-        // checks if there is time remaining and if not resets to default value
-      if (remainingTime === 0) {
-        clearInterval(timerInterval);
-        timerDisplay.innerHTML = "";
-        tempDisplay.innerHTML = `${initialTemp}°C`;
-        tempInput.defaultValue
-      } else {
-        remainingTime--;
-      }
-    }, 1000); 
+
+// Add event listener for popstate event to handle when user navigates back or forward
+window.addEventListener("popstate", () => {
+  clearInterval(countdownTimer);
+  loadUserData();
+});
 
     
   
-    // Add event listener for popstate event to handle when user navigates back or forward
-    window.addEventListener("load", () => {
-      clearInterval(countdownTimer);
-      loadUserData();
-    });
-
+    
 
   
   
